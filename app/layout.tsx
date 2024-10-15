00a78445b45ba5d93ca3d17f841ff0f4455872aa
Nico Albanese <nico.albanese@vercel.com>
2024-10-15 21:01:01 +0100

    updates

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import "./globals.css";
 import { GeistMono } from "geist/font/mono";
 import { GeistSans } from "geist/font/sans";
 import { ThemeProvider } from "next-themes";
+import { Toaster } from "@/components/ui/sonner";
+import { KasadaClient } from "@/utils/kasada/kasada-client";
 
 export const metadata = {
   metadataBase: new URL("https://natural-language-postgres.vercel.app"),
@@ -18,8 +20,10 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={`${GeistMono.className} ${GeistSans.className}`}>
+        <KasadaClient />
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           {children}
+          <Toaster />
         </ThemeProvider>
       </body>
     </html>
